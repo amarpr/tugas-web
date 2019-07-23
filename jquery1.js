5561fca22f2dc960eedbd0b2754e0ac23333a5d0
HendraLatumeten <52783772+HendraLatumeten@users.noreply.github.com>
2019-07-23 16:50:06 +0700

    Form daftar
    daftar dan jquery

---
+++ jquery1.js
@@ -0,0 +1,19 @@
+		 $(document).ready(function () {
+ 		$('#simpan').click(function(){
+ 		alert(" NIS: "     	+$('#input1').val() 
+ 		+ " nama: "       	+$('#input2').val()
+  		+ " Tanggal lahir:" +$('#input3').val()
+  		+ " Jenis Kelamin:" +$('#input4').val()
+  		+ " Alamat: " 		+$('#input5').val()
+  		+ " No Hp: "       	+$('#input6').val()
+  		+ " Agama: "        +$('#input7').val()
+  		+ " Nama ortu: "    +$('#input8').val()
+  		+ " Pekerjaan Ortu:"+$('#input9').val()
+ 		+ " Status Pendaftaran: "+$('#input10').val()
+ 		+ " Asal Sekolah: "  +$('#input11').val()
+		+ " Alamat Sekolah: "+$('#input12').val());
+		
+	
+	});
+ });
+
